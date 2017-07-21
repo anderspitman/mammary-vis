e5e87b30104e0d808fc09fed2efcec0edba96566
Juan J. Garcia Mesa <jgarc111@asu.edu>
2017-07-21 15:33:35 -0700

    Table with first columns of data

--- client/main.js
+++ client/main.js
@@ -12,10 +12,8 @@
                 url: "http://localhost:8080/metadata",
             })
             .then(function(genes) {
-                console.log(genes[0]),
-                GenesList.headers = genes[0],
-                GenesList.list = genes[1],
-                console.log(GenesList.list[0])
+                GenesList.headers = genes.column_names,
+                GenesList.list = genes.rows
             })
         }
     }
@@ -38,19 +36,47 @@
     var DataT = {
         oninit: GenesList.fetch,
         oncreate: function(vnode){
-            $(vnode.dom).append('<table id="genesTable" class="display"></table>');  
+            $(vnode.dom).append('<a class="toggle-vis" col="0">Ensembl Gene</a> - <a class="toggle-vis" col="1">Ensembl Transcript</a> - <a class="toggle-vis" col="2">Display Name</a> - <a class="toggle-vis" col="3">Reason</a> - <a class="toggle-vis" col="4">Codemlid</a>');
+            $(vnode.dom).append('<table id="genesTable" class="display"></table>');
+            $('a.toggle-vis').on('click',function(e){
+                console.log("e");
+                //Get the column object
+                var column = $('#genesTable').DataTable().column($(this).attr('col'));
+                //Toggle visibility
+                column.visible(!column.visible());
+                });
         },
         onupdate: function(vnode){
-            console.log(GenesList.list.ensembl_gene),
             $('#genesTable').DataTable({
                 data: GenesList.list,
+                "iDisplayLength": 25,
                 columnDefs: [
+                    {
+                        targets:"_all",
+                        className:"dt-center"
+                    },
                     {
                         targets: 0,
                         title:GenesList.headers[0],
                         render: function(data,type,row,meta){
-                            return '<a href="#!/'+data+'">'+data+'</a>';
-                        },
+                            return '<a href="#!/'+data+'" style="text-decoration: none"><div>'+data+'</div></a>';
+                        }
+                    },
+                    {
+                        targets: 1,
+                        title:GenesList.headers[1]
+                    },
+                    {
+                        targets: 2,
+                        title:GenesList.headers[2]
+                    },
+                    {
+                        targets: 3,
+                        title:GenesList.headers[3]
+                    },
+                    {
+                        targets:4,
+                        title:GenesList.headers[4]
                     }
                 ]
             });
